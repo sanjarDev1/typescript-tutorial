df37856d8c69fcd8a0ff4f1dff20b63d405a9f9f
sanjardev1 <sanjarbekqobulov02gmail.com>
2023-01-26 18:09:39 +0500

    Union

--- app.ts
+++ app.ts
@@ -1,25 +1,37 @@
-enum QuestionsStatus {
-  Published = 'published',
-  Draft = 'draft',
-  Deleted = 'deleted',
+const arr = ['sdf', 1];
+
+function logId(id: string | number | boolean) {
+  if (typeof id === 'string') {
+    console.log(id);
+  } else if (typeof id === 'number') {
+    console.log(id);
+  } else {
+    console.log(id);
+  }
 }
 
-async function getFaqs(req: {
-  topicId: number;
-  status: QuestionsStatus;
-}): Promise<
-  {
-    question: string;
-    answer: string;
-    tags: string[];
-    likes: number;
-    status: QuestionsStatus;
-  }[]
-> {
-  const res = await fetch('/faqs', {
-    method: 'POST',
-    body: JSON.stringify(req),
-  });
-  const data = await res.json();
-  return data;
+function logError(err: string | string[]) {
+  if (Array.isArray(err)) {
+    console.log(err);
+  } else {
+    console.log(err);
+  }
 }
+
+function logObject(obj: { a: number } | { b: number }) {
+  if ('a' in obj) {
+    console.log(obj.a);
+  } else {
+    console.log(obj.b);
+  }
+}
+
+function logMultipleIds(a: string | number, b: string | boolean) {
+  if (a === b) {
+  } else {
+    console.log(a);
+  }
+}
+
+let a:1 = 1
+a=1
