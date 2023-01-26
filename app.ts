0f5e8f9b6ba5fef0d2603f34d21e2dfd21ee4636
sanjardev1 <sanjarbekqobulov02gmail.com>
2023-01-26 17:29:33 +0500

    task

--- app.ts
+++ app.ts
@@ -1,38 +1,25 @@
-enum StatusCode {
-  SUCCESS = 1,
-  IN_PROCESS = 'p',
-  FAILED = 'f',
+enum QuestionsStatus {
+  Published = 'published',
+  Draft = 'draft',
+  Deleted = 'deleted',
 }
 
-const res = {
-  message: 'Sucsessfuly',
-  statusCode: StatusCode.SUCCESS,
-};
-
-if (res.statusCode === StatusCode.SUCCESS) {
-
+async function getFaqs(req: {
+  topicId: number;
+  status: QuestionsStatus;
+}): Promise<
+  {
+    question: string;
+    answer: string;
+    tags: string[];
+    likes: number;
+    status: QuestionsStatus;
+  }[]
+> {
+  const res = await fetch('/faqs', {
+    method: 'POST',
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+  return data;
 }
-
-function action(status: StatusCode){
-
-}
-
-action(StatusCode.SUCCESS);
-action(1); 
-action(3);
-
-function compute(){
-    return 3
-}
-
-enum Roles {
-    ADMIN = 1,
-    USER = compute()
-}
-
-// function test(x:{ADMIN:number}){
-
-// }
-
-// test(Roles)
-
