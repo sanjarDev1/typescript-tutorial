2091864f2a95e4ab0788ff3a7f02d05a6140e98f
sanjardev1 <sanjarbekqobulov02gmail.com>
2023-01-26 10:50:15 +0500

    tuples

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
 "use strict";
-const skills = ['Div', 'DevOps', 'Testing'];
-for (const skill of skills) {
-    console.log(skill.toLocaleLowerCase());
-}
-const res = skills
-    .filter((s) => s !== 'DevOps')
-    .map((s) => { return 1; })
-    .reduce((a, b) => a + b);
-console.log(res);
+const skills = [1, 'Devops', 'Testing'];
--- app.ts
+++ app.ts
@@ -1,11 +1,10 @@
-const skills: string[] = ['Div', 'DevOps', 'Testing'];
+const skills: [number, string, string] = [1, 'Devops', 'Testing'];
 
-for (const skill of skills) {
-  console.log(skill.toLocaleLowerCase());
-}
-const res = skills
-  .filter((s: string) => s !== 'DevOps')
-  .map((s) => {return 1})
-  .reduce((a, b) => a + b);
+const [id, skillName] = skills;
 
-console.log(res);
+const arr: [number, string, ...boolean[]] = [1, 'dfdf', true, false];
+// const id = skills[0];
+// const skillName = skills[1];
+
+// skills.push('salom');
+// skills.pop();
